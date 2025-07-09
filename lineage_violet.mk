6fa97896e57fea0f5ba68d223ec14bd34161ae89
Chronix <phoniexpaul88@gmail.com>
2025-07-09 15:39:46 +0530

    @EvoX Bringups

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -8,11 +8,21 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common EvoX stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit MiuiCamera
-$(call inherit-product, vendor/MiuiCamera/config.mk)
+#EvoX Flags
+EVO_BUILD_TYPE := Unofficial
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := false
+BYPASS_CHARGE_SUPPORTED  := true
+
+#Include ViperFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+#GAPPS
+WITH_GMS := true
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
