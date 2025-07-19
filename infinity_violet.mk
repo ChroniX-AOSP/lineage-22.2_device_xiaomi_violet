d6a0ea5b83dc7e9519ceef52fe968cbbce4f02ce
Chronix <phoniexpaul88@gmail.com>
2025-07-19 12:41:41 +0530

    Update infinity_violet.mk

--- infinity_violet.mk
+++ infinity_violet.mk
@@ -1,32 +1,39 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2018-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from violet device
-TARGET_DISABLE_EPPE := true
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common Infinityx stuff.
+# Inherit some common infinityFest AOSP stuff.
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
-#MIUI Camera
+# Infinity-X Flags
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := CHRONIX魚
+TARGET_SUPPORTS_BLUR := false
+TARGET_HAS_UDFPS := false
+WITH_GAPPS := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
+
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# MiuiCamera
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
-#Infinityx Flags
-INFINITY_MAINTAINER := CHRONIX 魚
-INFINITY_BUILD_TYPE := UNOFFICIAL
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_BLUR := true
-BYPASS_CHARGE_SUPPORTED := true
-PRODUCT_NO_CAMERA := true
-
-#GAPPS
-WITH_GMS := true
-WITH_GAPPS := true
+# Disable UDFPS animations ( unsupported )
+EXTRA_UDFPS_ICONS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := infinity_violet
@@ -36,3 +43,12 @@ PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc=$(call normalize-path-list, "violet-user 10 QKQ1.190915.002 V12.5.1.0 release-keys")
+
+    BUILD_FINGERPRINT := xiaomi/violet/violet:10/QKQ1.190915.002/V12.5.1.0.QFHINXM:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+   ro.build.fingerprint=$(BUILD_FINGERPRINT)
