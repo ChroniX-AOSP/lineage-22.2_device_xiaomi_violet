179d795295a93fe47e5392f7c978ff6d45af622e
Chronix <phoniexpaul88@gmail.com>
2025-10-14 19:05:29 +0530

    Track Necessary Repos

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+#Vendor && Kernel
+git clone https://github.com/LineageOS-Violet/vendor_xiaomi_violet.git -b ax vendor/xiaomi/violet
+git clone https://github.com/Joker-V2/kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
+
+#Firmware
+git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet
+
+#Hardware
+git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+
+#MemeCam
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
