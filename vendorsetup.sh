879562c362aebeec41756256cba3d97a477253c3
Chronix <phoniexpaul88@gmail.com>
2025-10-14 16:18:16 +0530

    Track Repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,14 @@
-git clone --depth=1 https://github.com/LineageOS-Violet/vendor_xiaomi_violet.git -b lineage-22.2 vendor/xiaomi/violet
-git clone https://github.com/ChroniX-AOSP/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
-git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
+#Vendor && Kernel Stuff
+git clone https://github.com/LineageOS-Violet/vendor_xiaomi_violet.git -b lineage-22.2 vendor/xiaomi/violet
+git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+
+# Hardware stuff
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+
+#Firmware
+git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet
+
+#BCR and Stuff
 git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
-git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
+
